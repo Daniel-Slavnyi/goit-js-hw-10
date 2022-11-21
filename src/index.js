16f12046ea9fd17444c52cf5f9f7f7ba82728ba7
Daniel-Slavnyi <dslavnyj00@gmail.com>
2022-11-21 19:17:35 +0200

    style

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('input');
 const countryListEl = document.querySelector('.country-list');
+const countryItemEl = document.querySelector('.country-info');
 const countryApiServices = new CountryApiServices();
 
 const onInputElInput = (e) => {
@@ -17,14 +18,15 @@ const onInputElInput = (e) => {
         countryApiServices.fetchPosts().then(data => {
             console.log(data);
             if (data.length > 10) {
+                countryListEl.innerHTML = '';
                 Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
             }
 
             return data;
         }).then(data => {
-            if (data.length > 2 && data.length <= 10) {
+            if (data.length >= 2 && data.length <= 10) {
                 let countryList = data.map(el => {
-                  return  `<li class="country-">
+                  return  `<li class="country-item">
                         <img class="country-img" src="${el.flags.svg}" alt="${el.name}">
                         <p class="country-name">
                             ${el.name}
@@ -32,10 +34,34 @@ const onInputElInput = (e) => {
                     </li>`;
                 }).join('');
                 console.log(countryList);
+                
+                countryListEl.innerHTML = '';
+                countryItemEl.innerHTML = '';
                 countryListEl.innerHTML = countryList;
             }
+            return data;
+        }).then(data => {
+            if (data.length === 1) {
+                countryListEl.innerHTML = '';
+                countryItemEl.innerHTML =
+                    `<img src="${data[0].flags.png}" alt=${data[0].name}>
+                    <div>
+                        <h2 class="cuntry-title">${data[0].name}</h2>
+                        <p class="country-capital">
+                        Capital: ${data[0].capital}
+                        </p>
+                        <p class="country-population">
+                        Population: ${data[0].population}
+                        </p>
+                        <p class="country-languages">
+                        Languages: ${data[0].languages.map(el => el.name).join('')}
+                        </p>
+                    </div>`;
+            }
         }).catch(arr => {
-            Notiflix.Notify.warning("Oops, there is no country with that name");
+            countryListEl.innerHTML = '';
+            countryItemEl.innerHTML = '';
+            Notiflix.Notify.failure("Oops, there is no country with that name");
         });
     }
     
